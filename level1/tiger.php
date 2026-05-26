a9478c2f6fe444a8167bd228b24d74d9cf124efe
Edwin <edwin.barrera1025@gmail.com>
2026-05-26 12:40:08 +0200

    First Commit: Level1

---
+++ level1/tiger.php
@@ -0,0 +1,14 @@
+<?php
+class Tigger
+{
+
+    private function __construct()
+    {
+        echo "Building character..." . PHP_EOL;
+    }
+
+    public function roar()
+    {
+        echo "Grrr!" . PHP_EOL;
+    }
+}
